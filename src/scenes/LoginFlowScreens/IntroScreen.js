4468dbc2eacafda4c03cc9462ac8d88bfd9fa110
Mani Kant Tiwari <manikanttiwari8@gmail.com>
2021-07-12 01:46:25 +0530

    Added structured files

---
+++ src/scenes/LoginFlowScreens/IntroScreen.js
@@ -0,0 +1,9 @@
+import * as React from 'react';
+import { View, Text } from "react-native"
+
+import {Colors} from '../../styles/index'
+
+const IntroScreen = (props)=>{
+    return null
+}
+export default  IntroScreen
